ebb6171e93d1c5777f7a7b7bf5d449373ec835f3
Honghao Qiu <3158874848@qq.com>
2020-08-25 22:55:55 +0800

    fix some bug

---
+++ MinicarMuseum-ejb/src/java/repository/JPAPersonRepositoryImpl.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package repository;
+
+import entities.Person;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author 31588
+ */
+public class JPAPersonRepositoryImpl implements PersonRepository{
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public void addPerson(Person person) throws Exception {
+        entityManager.persist(person);
+        
+    }
+
+    @Override
+    public Person searchPersonById(int id) throws Exception {
+       Person person = entityManager.find(Person.class, id);
+       return person;
+    }
+
+    @Override
+    public Person searchPersonByEmailAndPassword(String email, String password) throws Exception {
+       Person person=(Person)entityManager.createNamedQuery("Person.findByEmailAndPassword").setParameter(email, email).setParameter(password, password).getSingleResult();
+       return person;
+    }
+}
