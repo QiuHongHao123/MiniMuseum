2b6245c23ee61c4b1e302034217f22d844d16e28
Honghao Qiu <3158874848@qq.com>
2020-08-26 15:32:28 +0800

    ...

--- MinicarMuseum-war/src/java/controller/PersonController.java
+++ MinicarMuseum-war/src/java/controller/PersonController.java
@@ -5,10 +5,52 @@
  */
 package controller;
 
+import Model.PersonModel;
+import entities.Person;
+import javax.el.ELContext;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import mbeans.PersonManagedBean;
+
 /**
  *
  * @author 31588
  */
+@RequestScoped
+@Named("personController")
 public class PersonController {
+    @ManagedProperty(value ="#{personManagedBean}")
+    PersonManagedBean personManagedBean;
+    private PersonModel personModel;
+
+    public PersonModel getPersonModel() {
+        return personModel;
+    }
+
+    public void setPersonModel(PersonModel personModel) {
+        this.personModel = personModel;
+    }
+    
+    public PersonController(){
+        ELContext elContext = FacesContext.getCurrentInstance().getELContext();
+        personManagedBean = (PersonManagedBean) FacesContext.getCurrentInstance().getApplication()
+                .getELResolver().getValue(elContext, null, "personManagedBean");
+    }
+    public void register(PersonModel personModel){
+        try{
+            Person  person = new Person();
+            person.setName(personModel.getName());
+            person.setAge(personModel.getAge());
+            person.setEmail(personModel.getEmail());
+            person.setPassword(personModel.getPassword());
+            person.setGender(personModel.getGender());
+            personManagedBean.addPerson(person);
+        }
+        catch(Exception ex){
+        }
+        
+    }
     
 }
