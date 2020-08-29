9a9069c0c2f3a5d498d894bd3553c28d92384e9f
Honghao Qiu <3158874848@qq.com>
2020-08-29 10:07:22 +0800

    complete controller

--- MinicarMuseum-war/src/java/controller/CollectionController.java
+++ MinicarMuseum-war/src/java/controller/CollectionController.java
@@ -5,10 +5,37 @@
  */
 package controller;
 
+import Model.CollectionModel;
+import javax.el.ELContext;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import mbeans.CollectionManagedBean;
+import mbeans.PersonManagedBean;
+
 /**
  *
  * @author 31588
  */
+@Named("collectionController")
+@RequestScoped
 public class CollectionController {
+    @ManagedProperty(value = "#{collectionManagedBean}")
+    CollectionManagedBean collectionManagedBean;
+    private CollectionModel collectionModel;
+
+    public CollectionModel getCollectionModel() {
+        return collectionModel;
+    }
+
+    public void setCollectionModel(CollectionModel collectionModel) {
+        this.collectionModel = collectionModel;
+    }
+    public CollectionController(){
+        ELContext elContext = FacesContext.getCurrentInstance().getELContext();
+        collectionManagedBean = (CollectionManagedBean) FacesContext.getCurrentInstance().getApplication()
+                .getELResolver().getValue(elContext, null, "collectionManagedBean");
+    }
     
 }
--- MinicarMuseum-war/src/java/controller/MinicarController.java
+++ MinicarMuseum-war/src/java/controller/MinicarController.java
@@ -5,10 +5,36 @@
  */
 package controller;
 
+import Model.MinicarModel;
+import javax.el.ELContext;
+import javax.enterprise.context.RequestScoped;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import mbeans.MinicarManagedBean;
+
 /**
  *
  * @author 31588
  */
+@Named("minicarController")
+@RequestScoped
 public class MinicarController {
-    
+     @ManagedProperty(value ="#{minicarManagedBean}")
+     MinicarManagedBean minicarManagedBean;
+     private MinicarModel minicarModel;
+
+    public MinicarModel getMinicarModel() {
+        return minicarModel;
+    }
+
+    public void setMinicarModel(MinicarModel minicarModel) {
+        this.minicarModel = minicarModel;
+    }
+    public MinicarController(){
+         ELContext elContext = FacesContext.getCurrentInstance().getELContext();
+        minicarManagedBean = (MinicarManagedBean) FacesContext.getCurrentInstance().getApplication()
+                .getELResolver().getValue(elContext, null, "minicarManagedBean");
+    }
 }
+    
